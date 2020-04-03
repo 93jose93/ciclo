ff3eba8cb20a20b67c25d26f6bce6f62fd0d068d
jose <kcorjose78@gmail.com>
2020-04-02 22:41:11 -0400

    se agrego el campo para mostrar alerta, el formulario para agregar, y evalue en los input para traer datos que se hallan enviado

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,37 +15,70 @@
         <div class="container">
            <div class="row">
                 <div class="col-sm8 mx-auto">
+                    <div class="card border-0 shadow">
+                       <div class="card-body">
+                            @if($errors->any())
+                            <div class="alert alert-danger">
+                                   @foreach($errors->all() as $error)
+                                   - {{ $error }} <br>
+                                   @endforeach
+                            </div>
+                            @endif
+                            <form action="{{ route('users.store') }}" method="POST">
+                            
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                            <input type="text" name="name" class="form-control" placeholder="nombre" value="{{ old('name') }}">
+                                            
+                                    </div>
+                                    <div class="col-sm-4">
+                                            <input type="text" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}"> 
+                                    </div>
+                                    <div class="col-sm-3">
+                                            <input type="password" name="password" class="form-control" placeholder="Contraseña"> 
+                                    </div>
+                                    <div class="col-auto">
+                                    @csrf 
+                                    <button type="submit" class="btn btn-primary">
+                                    Enviar
+                                    </button>
+                                    </div>
+                                </div>
+                            </form>
+                       </div>
+                    </div>
+                   
 
-                <table class="table">
-                    <thead>
-                       <tr>
-                         <th>ID</th>
-                         <th>Nombre</th>
-                         <th>Email</th>
-                         <th>&nbsp;</th>
-                       </tr>
-                    </thead>
-                    <tbody>
-                          @foreach($users as $user)
-                          <tr>
-                               <td>{{ $user->id }}</td>
-                               <td>{{ $user->name }}</td>
-                               <td>{{ $user->email }}</td>
-                               <td>
-                                  <form action="{{ route('users.destroy', $user) }}" method="POST">
-                                       @method('DELETE')
-                                       @csrf
-                                       <input 
-                                       type="submit" 
-                                       value="Eliminar" 
-                                       class="btn btn-sm btn-danger" onclick="return confirm('¿Desea eliminar... ?')">
-                                  </form>
-                               </td>
-                          </tr>
-                          @endforeach
-                    </tbody>
-                </table>
-                </div>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input 
+                                        type="submit" 
+                                        value="Eliminar" 
+                                        class="btn btn-sm btn-danger" onclick="return confirm('¿Desea eliminar... ?')">
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>      
            </div>
         </div>
     </body>
